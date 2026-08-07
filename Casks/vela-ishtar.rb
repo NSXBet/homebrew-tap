f86cd809f93338f7162ab86eda38602798e3fe13
Daniel <daniel.dutu@nsx.bet>
2026-08-08 00:16:11 +0300

    vela-ishtar 0.4.3

--- Casks/vela-ishtar.rb
+++ Casks/vela-ishtar.rb
@@ -1,6 +1,6 @@
 cask "vela-ishtar" do
-  version "0.4.2"
-  sha256 "2cc9941e1bd783dc1a7e11f0f12112ad5fb7aaf6adfffb1c4de1c3e54ace3267"
+  version "0.4.3"
+  sha256 "7db044b5c25469080827a630802c5e2f09b5b49d995099137ea3e8d3e0f554f4"
 
   url "https://github.com/NSXBet/vela-ishtar/releases/download/v#{version}/VelaIshtar-#{version}.zip"
   name "Vela Ishtar"
