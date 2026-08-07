fc1a69ff8ecff64ef96bafd9ed90cfd459578f74
Daniel <daniel.dutu@nsx.bet>
2026-08-08 01:40:35 +0300

    vela-ishtar 0.4.3: refresh sha256 for republished asset

--- Casks/vela-ishtar.rb
+++ Casks/vela-ishtar.rb
@@ -1,6 +1,6 @@
 cask "vela-ishtar" do
   version "0.4.3"
-  sha256 "7db044b5c25469080827a630802c5e2f09b5b49d995099137ea3e8d3e0f554f4"
+  sha256 "55fe177ff50ce5dce7cbfb43b87e18574c76f1becde5d913146562ac48af9ecc"
 
   url "https://github.com/NSXBet/vela-ishtar/releases/download/v#{version}/VelaIshtar-#{version}.zip"
   name "Vela Ishtar"
